5bdccf80920afe6b1132977ebd4e1bc7fa9163f5
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-05-05 11:46:26 +0300

    updates

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello There!");
 		System.err.println("This is the first project using spring boot");
-		// SpringApplication.run(DemoApplication.class, args);
 	}
 
 }

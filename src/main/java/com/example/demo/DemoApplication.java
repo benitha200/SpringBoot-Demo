94a3bc7af569058d64becae6cb252c9e0c937ac3
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-05-05 12:09:33 +0300

    expose the application on web

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello There!");
-		System.err.println("This is the first project using spring boot");
+		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
